fc054e07a5eda8b6dfffea31242a251222b0d35e
kismatkc <kismatkc28@gmail.com>
2025-03-08 00:00:24 -0500

    auto

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
-import { Text, View,TouchableOpacity  } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import Sleep from "~/components/sleep";
-import { topics,topicsType } from "~/constants";
+import { topics, topicsType } from "~/constants";
 const Home = ({}) => {
-  const [view, setView] = useState<topicsType| null>("Sleep");
-  if(view === "Sleep") return <Sleep />
+  const [view, setView] = useState<topicsType | null>("Sleep");
+  // const [view, setView] = useState<topicsType | null>(null);
+  if (view === "Sleep") return <Sleep />;
   return (
     <View className="flex flex-col h-full px-2 gap-y-3">
       {topics.map((subject) => (
-        <TouchableOpacity className="bg-[#F3EFE0] w-full h-[20%] rounded-3xl flex items-center justify-center" key={subject} onPress={()=> setView(subject)} >
+        <TouchableOpacity
+          className="bg-[#F3EFE0] w-full h-[20%] rounded-3xl flex items-center justify-center"
+          key={subject}
+          onPress={() => setView(subject)}
+        >
           <Text className="text-3xl font-bold ">{subject}</Text>
-        </TouchableOpacity >
+        </TouchableOpacity>
       ))}
     </View>
   );
--- components/sleep.tsx
+++ components/sleep.tsx
@@ -1,6 +1,21 @@
-import { format,add } from "date-fns";
+import { format, add } from "date-fns";
 import { useEffect, useState } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, Button } from "react-native";
+
+import { Linking } from "react-native";
+import TimePickerComponent from "./time-picker";
+
+const setAlarm = (date: Date) => {
+  const localDateString = format(date, "hh:mm aa");
+  const shortcutURL = `shortcuts://run-shortcut?name=alarm&input=${encodeURIComponent(
+    localDateString
+  )}`;
+
+  Linking.openURL(shortcutURL).catch((err) =>
+    console.error("Error opening shortcut:", err)
+  );
+};
+
 const Sleep = ({}) => {
   const [time, setTime] = useState<Date>(new Date());
 
@@ -19,16 +34,60 @@ const Sleep = ({}) => {
         <Text className="text-4xl font-bold">
           Today is {format(time, "MMM, dd yyyy")}
         </Text>
-        <Text className="text-4xl font-bold">
-          Time is {add()}
-
+      </View>
+      <View className="mt-6 flex-col gap-y-2">
+        <Text className="text-3xl font-bold text-center mb-3">
+          Ideal section
         </Text>
+        <View className="flex flex-row gap-x-1 items-center justify-between">
+          <View className="flex flex-row gap-x-1">
+            <Text className="text-xl font-bold">For 8hrs 10mins sleep:</Text>
+            <Text className="text-xl font-bold ">
+              {format(add(time, { hours: 8, minutes: 10 }), "hh:mm aa")}
+            </Text>
+          </View>
+
+          <Button
+            title="Set alarm"
+            color="green"
+            onPress={() => setAlarm(add(time, { hours: 8, minutes: 10 }))}
+          />
+        </View>
+        <View className="flex flex-row gap-x-1 items-center justify-between">
+          <View className="flex flex-row gap-x-1">
+            <Text className="text-xl font-bold">For 8hrs 13mins sleep:</Text>
+            <Text className="text-xl font-bold ">
+              {format(add(time, { hours: 8, minutes: 13 }), "hh:mm aa")}
+            </Text>
+          </View>
+
+          <Button
+            title="Set alarm"
+            color="green"
+            onPress={() => setAlarm(add(time, { hours: 8, minutes: 13 }))}
+          />
+        </View>
+        <View className="flex flex-row gap-x-1 items-center justify-between">
+          <View className="flex flex-row gap-x-1">
+            <Text className="text-xl font-bold">For 8hrs 16mins sleep:</Text>
+            <Text className="text-xl font-bold ">
+              {format(add(time, { hours: 8, minutes: 16 }), "hh:mm aa")}
+            </Text>
+          </View>
+
+          <Button
+            title="Set alarm"
+            color="green"
+            onPress={() => setAlarm(add(time, { hours: 8, minutes: 16 }))}
+          />
+        </View>
       </View>
-      <View className="flex flex-col mt-6">
-        <Text className="text-4xl font-bold">For 8hrs 10mins sleep</Text>
-        <Text className="text-4xl font-bold">For 8hrs 10mins sleep</Text>
-        
-      </View>
+      <TimePickerComponent
+        date={time}
+        setAlarm={(date) => {
+          setAlarm(date);
+        }}
+      />
     </View>
   );
 };
